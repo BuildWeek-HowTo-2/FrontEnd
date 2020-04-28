1cc8ed315c1b498a31b00a18d06118e528fe9596
NomadDaniel <reachingdaniel@gmail.com>
2020-04-28 12:16:46 -0400

    merging to predeployment

--- howto-2-react-app/src/components/Login.js
+++ howto-2-react-app/src/components/Login.js
@@ -83,7 +83,7 @@ export default function Login() {
 
   return (
     <form onSubmit={formSubmit}>
-      <h2>Sign In</h2>
+      <h2>Login</h2>
       <label htmlFor="username">
       User Name 
       <input
